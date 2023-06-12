0a6f9362e956ae0af0f98cd8e3ea49b40d2c18db
vinisooo <cviniciusliramdev@gmail.com>
2023-06-12 18:57:16 -0300

    fix: remove not used import

--- src/services/user/register.services.ts
+++ src/services/user/register.services.ts
@@ -3,7 +3,6 @@ import { User } from "../../entities/user.entity";
 import { Address } from "../../entities/address.entity";
 import { Repository } from "typeorm";
 import { tAddressRegisterSchema } from "../../schemas/address.schemas";
-import { noPasswordUserSchema } from "../../schemas/user.schemas";
 
 const registerUserService = async(data: User) => {
     const userRepository: Repository<User> = AppDataSource.getRepository(User);
