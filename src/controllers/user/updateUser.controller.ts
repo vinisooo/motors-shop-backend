a54cb919e1093f44b0adf242c2610f0bc72ba319
Roberto Pontes <robertopnts13@gmail.com>
2023-06-20 16:27:23 -0300

    feat: update user feature concluded

---
+++ src/controllers/user/updateUser.controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+import updateUserService from "../../services/user/updateUser.services";
+
+const updateUserController = async (req: Request, res: Response) => {
+    const userId = req.loggedUser.id
+    const dataToUpdate = req.body
+    const updatedUser = await updateUserService(dataToUpdate, userId)
+
+    return res.send(updatedUser)
+}
+
+export default updateUserController
