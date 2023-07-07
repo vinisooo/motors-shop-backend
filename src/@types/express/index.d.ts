cf4368f437cd342860284898ea0319759d0478a1
vinisooo <cviniciusliramdev@gmail.com>
2023-07-07 12:39:41 -0300

    fix: adjust files pattern

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -2,11 +2,12 @@ import { Advertisement } from "../../entities/advertisement.entity";
 import { User } from "../../entities/user.entity";
 import { Address } from "../../entities/address.entity";
 import { Comment } from "../../entities/comment.entity";
+import { GalleryAdvertisement } from "../../entities/galleryAdvertisement.entity";
 
 declare global {
     namespace Express {
         interface Request {
-            foundById: any,
+            foundById: Comment | Address | User | Advertisement | GalleryAdvertisement,
             loggedUser: {isAdvertiser: boolean, id: string}
         }
     }
